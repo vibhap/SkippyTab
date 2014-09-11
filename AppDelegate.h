e0cab6cbffdecad9c4d15343b69b0e85dc5b4382
Vibha Prahlada <vibha.prahlada@gmail.com>
2014-09-10 23:07:24 -0700

    Added the appdelegate files

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SkippyTab
+//
+//  Created by Vibha prahlada and Namratha Bharadwaj on 3/7/14.
+//  Copyright (c) 2014 Vibha prahlada and Namratha Bharadwaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
